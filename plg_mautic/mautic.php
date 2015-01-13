6d2ce231e4cd92641c146eea5190e50e92848b17
George Wilson <georgejameswilson@googlemail.com>
2015-01-13 13:30:15 +0000

    Code style cleanup

--- plg_mautic/mautic.php
+++ plg_mautic/mautic.php
@@ -14,23 +14,23 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 jimport( 'joomla.plugin.plugin' );
 
 /**
+ * The Mautic plugin for Joomla
  *
- * @package		Joomla
- * @subpackage	System.Mautic
+ * @package     Joomla
+ * @subpackage  System.Mautic
  */
-class plgSystemMautic extends JPlugin
+class PlgSystemMautic extends JPlugin
 {
 	/**
 	 * This event is triggered after the framework has loaded and the application initialise method has been called.
 	 *
-	 * @return	void
-	 *
+	 * @return   void
 	 */
 	public function onAfterDispatch()
 	{
-		$app 		= JFactory::getApplication();
-		$document 	= JFactory::getDocument();
-		$input		= $app->input;
+		$app       = JFactory::getApplication();
+		$document  = JFactory::getDocument();
+		$input     = $app->input;
 
 		// Check to make sure we are loading an HTML view and there is a main component area
 		if ($document->getType() !== 'html' || $input->get('tmpl', '', 'cmd') === 'component' || $app->isAdmin())
@@ -38,38 +38,38 @@ class plgSystemMautic extends JPlugin
 			return true;
 		}
 
-        // Get additional data to send
-        $attrs = array();
-        $attrs['title'] = $document->title;
-        $attrs['language'] = $document->language;
-        $attrs['referrer'] = $_SERVER['HTTP_REFERER'];
-        $attrs['url'] = JURI::base();
+		// Get additional data to send
+		$attrs = array();
+		$attrs['title'] = $document->title;
+		$attrs['language'] = $document->language;
+		$attrs['referrer'] = $_SERVER['HTTP_REFERER'];
+		$attrs['url'] = JUri::base();
 
-        $user = JFactory::getUser();
+		$user = JFactory::getUser();
 
-        // Get info about the user if logged in
-        if (!$user->guest)
-        {
-            $attrs['email'] = $user->email;
+		// Get info about the user if logged in
+		if (!$user->guest)
+		{
+			$attrs['email'] = $user->email;
 
-            $name = explode(' ', $user->name);
+			$name = explode(' ', $user->name);
 
-            if (isset($name[0]))
-            {
-                $attrs['firstname'] = $name[0];
-            }
+			if (isset($name[0]))
+			{
+				$attrs['firstname'] = $name[0];
+			}
 
-            if (isset($name[count($name) - 1]))
-            {
-                $attrs['lastname'] = $name[count($name) - 1];
-            }
-        }
+			if (isset($name[count($name) - 1]))
+			{
+				$attrs['lastname'] = $name[count($name) - 1];
+			}
+		}
 
 		$encodedAttrs = urlencode(base64_encode(serialize($attrs)));
 
-        $buffer  = $document->getBuffer('component');
-        $image   = '<img src="' . $this->params->get('base_url') . '/p/mtracking.gif?d=' . $encodedAttrs . '" />';
-        $buffer .= $image;
+		$buffer  = $document->getBuffer('component');
+		$image   = '<img src="' . $this->params->get('base_url') . '/p/mtracking.gif?d=' . $encodedAttrs . '" />';
+		$buffer .= $image;
         
 		$document->setBuffer($buffer, 'component');
 
@@ -77,52 +77,52 @@ class plgSystemMautic extends JPlugin
 	}
 
 	/**
-     * Insert form script to the content
-     *
-     * @param	string	The context of the content being passed to the plugin.
-     * @param	object	The article object.  Note $article->text is also available
-     * @param	object	The article params
-     * @param	int	The 'page' number
-     */
-    public function onContentPrepare($context, &$article, &$params, $page = 0)
-    {
-    	$app 		= JFactory::getApplication();
-		$document 	= JFactory::getDocument();
-		$input		= $app->input;
+	 * Insert form script to the content
+	 *
+	 * @param	string	The context of the content being passed to the plugin.
+	 * @param	object	The article object.  Note $article->text is also available
+	 * @param	object	The article params
+	 * @param	int	The 'page' number
+	 */
+	public function onContentPrepare($context, &$article, &$params, $page = 0)
+	{
+		$app      = JFactory::getApplication();
+		$document = JFactory::getDocument();
+		$input    = $app->input;
 
-    	// Check to make sure we are loading an HTML view and there is a main component area and content is not being indexed
-        if ($document->getType() !== 'html' 
-        	|| $input->get('tmpl', '', 'cmd') === 'component' 
-        	|| $app->isAdmin() 
-        	|| $context == 'com_finder.indexer')
-        {
-            return true;
-        }
+		// Check to make sure we are loading an HTML view and there is a main component area and content is not being indexed
+		if ($document->getType() !== 'html' 
+			|| $input->get('tmpl', '', 'cmd') === 'component' 
+			|| $app->isAdmin() 
+			|| $context == 'com_finder.indexer')
+		{
+			return true;
+		}
 
-        // simple performance check to determine whether bot should process further
-        if (strpos($article->text, '{mauticform') === false)
-        {
-            return true;
-        }
+		// Simple performance check to determine whether bot should process further
+		if (strpos($article->text, '{mauticform') === false)
+		{
+			return true;
+		}
 
-        // expression to search for (positions)
-        $regex		= '/{mauticform\s+(.*?)}/i';
+		// Expression to search for (positions)
+		$regex = '/{mauticform\s+(.*?)}/i';
 
-        // Find all instances of plugin and put in $matches for githubrepo
-        // $matches[0] is full pattern match, $matches[1] is the repo declaration
-        preg_match_all($regex, $article->text, $matches, PREG_SET_ORDER);
+		// Find all instances of plugin and put in $matches for githubrepo
+		// $matches[0] is full pattern match, $matches[1] is the repo declaration
+		preg_match_all($regex, $article->text, $matches, PREG_SET_ORDER);
 
-        if ($matches && isset($matches[0]))
-        {
-            foreach ($matches as $match)
-            {
-            	if (isset($match[1]))
-            	{
-            		$formId = (int) $match[1];
-	                $formTag = '<script type="text/javascript" src="' . $this->params->get('base_url') . '/p/form/generate.js?id=' . $formId . '"></script>';
-	                $article->text = str_replace($match[0], $formTag, $article->text);
-            	}
-            }
-        }
-    }
+		if ($matches && isset($matches[0]))
+		{
+			foreach ($matches as $match)
+			{
+				if (isset($match[1]))
+				{
+					$formId = (int) $match[1];
+					$formTag = '<script type="text/javascript" src="' . $this->params->get('base_url') . '/p/form/generate.js?id=' . $formId . '"></script>';
+					$article->text = str_replace($match[0], $formTag, $article->text);
+				}
+			}
+		}
+	}
 }
